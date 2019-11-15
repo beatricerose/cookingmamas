24d7025d45bc6d9610c99cabb98725f552c4494d
Alison Jacobs <alison@jacobsfamilyla.com>
2019-11-15 13:34:11 +0100

    more on the random spawner:)

--- Assets/Scripts/RandomSpawner.cs
+++ Assets/Scripts/RandomSpawner.cs
@@ -7,7 +7,9 @@ public class RandomSpawner : MonoBehaviour
    // public GameObject food;
     public List<GameObject> list;
     bool startnext = true;
+    public float spawnWidth = 1;
     // Start is called before the first frame update
+    
     void Start()
     {
         
@@ -24,15 +26,27 @@ public class RandomSpawner : MonoBehaviour
 
         //do not need to respect the cmaera's position
         //look at asteroid spawner from the first lab to see how this works :) 
-        Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(600, Screen.height), Camera.main.farClipPlane / 2));
+        //Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(600, Screen.height), Camera.main.farClipPlane / 2));
+        Vector3 spawnPosition = transform.position;
         //gonna need to get the position is it just the top of the screen?
         //can create a void bar on the top of the screen and put it there?
-        GameObject copyfood = Instantiate(sprite, screenPosition, Quaternion.identity);
+        //GameObject copyfood = Instantiate(sprite, screenPosition, Quaternion.identity);
+        //spawnPosition += new Vector3(Random.Range(-spawnWidth, spawnWidth), 0, 0);
+        //spawnPosition += new Vector3(Random.Range(-Screen.width, Screen.width), 0, 0);
+        spawnPosition += new Vector3(Random.Range(-spawnWidth, spawnWidth), 0, 0);
+        GameObject copyfood = Instantiate(sprite, spawnPosition, Quaternion.identity);
         Rigidbody rb2d = copyfood.GetComponent<Rigidbody>();
 
         rb2d.velocity = new Vector3(1, 1,1) * 5;
+        //destory(copyfood);
+
         startnext = true;
     }
+    //void destory(GameObject food)
+    //{
+        //if (rb2d.position.y < 0){
+           // destoryObject(food); 
+       // }    //}
     //figure out why it is swp
 
     // Update is called once per frame
