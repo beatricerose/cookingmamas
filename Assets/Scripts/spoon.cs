f9309b386203d2f47ca0ff80f00e060e09b65d8d
Delaney Sears <delaney.sears@gmail.com>
2019-11-17 14:59:35 +0100

    spoon + prefabs made convex

---
+++ Assets/Scripts/spoon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class spoon : MonoBehaviour
+{
+    bool turn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+      
+
+        
+            turn = true;
+            if (turn == true)
+            {
+                Vector3 rotation = new Vector3(0, 360, 0);
+                transform.Rotate(rotation);
+            }
+            turn = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+}
