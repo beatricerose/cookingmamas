d70bb8df29d2dfeb36ca119c89c4811707ff4e43
Liya <17liyaw@gmail.com>
2019-11-19 11:17:42 +0100

    bowl + collection + collectable

---
+++ Assets/Scripts/Flour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flour : Collectable
+{
+    public Flour()
+    {
+        title = "flour";
+    }
+
+    
+}
