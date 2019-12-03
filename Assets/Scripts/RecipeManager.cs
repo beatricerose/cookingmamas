51e5f511bc6e0d71caf92148cf8901b7493868d4
Bea Rose <bearose12@gmail.com>
2019-12-03 11:07:37 +0100

    Donut/Macaron Recipes added

--- Assets/Scripts/RecipeManager.cs
+++ Assets/Scripts/RecipeManager.cs
@@ -9,8 +9,10 @@ public class RecipeManager : MonoBehaviour
     public static Recipe cookie = new Recipe(3, 2, 1, 3, 2);
     public static Recipe cake = new Recipe(3, 2, 2, 5, 2);
     public static Recipe brownie = new Recipe(3, 2, 1, 5, 2);
-    public static Recipe[] recipeList = { cookie, cake, brownie };
-    private static string[] cookingSceneList = { "CookieScene", "CakeScene", "BrownieScene"};
+    public static Recipe donut = new Recipe(3, 3, 3, 0, 2);
+    public static Recipe macaron = new Recipe(4, 3, 0, 2, 6);
+    public static Recipe[] recipeList = { cookie, cake, brownie, donut, macaron };
+    private static string[] cookingSceneList = { "CookieScene", "CakeScene", "BrownieScene", "DonutScene", "MacaronScene"};
     public static bool recipeDone;
 
     private void Awake()
@@ -31,7 +33,7 @@ public class RecipeManager : MonoBehaviour
     // Start is called before the first frame update --> can't call if not Singleton (all static)
     void Start()
     {
-      
+       
 	}
 
     // Update is called once per frame
